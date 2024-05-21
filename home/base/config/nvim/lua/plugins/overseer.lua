481c7fe3f41d348c8da8e29dfc021757a70f4418
Rasmus Eneman <rasmus@eneman.eu>
2024-05-21 15:18:20 +0200

    nvim: Overseer nx template

--- home/base/config/nvim/lua/plugins/overseer.lua
+++ home/base/config/nvim/lua/plugins/overseer.lua
@@ -13,7 +13,94 @@ return {
         end
         return vim.fs.find(is_dofile, { upward = true, type = 'file', path = opts.dir })[1]
       end
+      local function get_nxconfig(opts)
+        local is_nxconfig = function(name)
+          name = name:lower()
+          return name == 'nx.json'
+        end
+        return vim.fs.find(is_nxconfig, { upward = true, type = 'file', path = opts.dir })[1]
+      end
 
+      local nx = {
+        name = 'nx',
+        cache_key = function(opts)
+          return get_nxconfig(opts)
+        end,
+        condition = {
+          callback = function(opts)
+            if vim.fn.executable 'nx' == 0 then
+              return false, 'Command "nx" not found'
+            end
+            if not get_nxconfig(opts) then
+              return false, 'No nx.json found'
+            end
+            return true
+          end,
+        },
+        generator = function(opts, cb)
+          local ret = {}
+          local function nx(args, cbi)
+            local jobid = vim.fn.jobstart({ 'nx', 'show', '--json', table.unpack(args) }, {
+              cwd = opts.dir,
+              stdout_buffered = true,
+              on_stdout = vim.schedule_wrap(function(j, output)
+                local ok, data = pcall(vim.json.decode, table.concat(output, ''))
+                if not ok then
+                  log:error('nx produced invalid json: %s\n%s', data, output)
+                  cbi(nil)
+                  return
+                end
+                assert(data)
+                cbi(data)
+              end),
+            })
+            if jobid == 0 then
+              log:error "Passed invalid arguments to 'nx'"
+              cbi(nil)
+            elseif jobid == -1 then
+              log:error "'nx' is not executable"
+              cbi(nil)
+            end
+          end
+
+          nx({ 'projects' }, function(projects)
+            if projects == nil then
+              cb(ret)
+              return
+            end
+
+            local active = 0
+            for _, project in ipairs(projects) do
+              active = active + 1
+              nx({ 'project', project }, function(data)
+                if data == nil then
+                  goto continue
+                end
+                for target_name, target in pairs(data.targets) do
+                  table.insert(ret, {
+                    name = string.format('nx run %s:%s', project, target_name),
+                    -- desc = recipe.doc,
+                    -- priority = k == data.first and 55 or 60,
+                    priority = 60,
+                    params = {},
+                    builder = function(params)
+                      local cmd = { 'nx', 'run', string.format('%s:%s', project, target_name) }
+                      return {
+                        cmd = cmd,
+                      }
+                    end,
+                  })
+                end
+                ::continue::
+                active = active - 1
+                if active == 0 then
+                  cb(ret)
+                end
+              end)
+            end
+          end)
+        end,
+      }
       local dofile = {
         name = 'dofile',
         cache_key = function(opts)
@@ -68,7 +155,7 @@ return {
                 table.insert(ret, {
                   name = string.format('do %s', recipe.name),
                   desc = recipe.doc,
-                  priority = k == data.first and 55 or 60,
+                  priority = k == data.first and 25 or 30,
                   params = params_defn,
                   builder = function(params)
                     local cmd = { 'do', recipe.name }
@@ -103,6 +190,7 @@ return {
       }
 
       overseer.register_template(dofile)
+      overseer.register_template(nx)
       overseer.setup()
     end,
   },
