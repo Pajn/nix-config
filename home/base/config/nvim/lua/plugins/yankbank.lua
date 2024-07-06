eee89049d841a221a1b044045aef8f33567c5504
Rasmus Eneman <rasmus@eneman.eu>
2024-07-06 20:40:57 +0200

    nvim: Yankbank config

--- home/base/config/nvim/lua/plugins/yankbank.lua
+++ home/base/config/nvim/lua/plugins/yankbank.lua
@@ -1,6 +1,9 @@
 return {
   'ptdewey/yankbank-nvim',
   config = function()
-    require('yankbank').setup()
+    require('yankbank').setup {
+      max_entries = 19,
+      num_behavior = 'jump',
+    }
   end,
 }
