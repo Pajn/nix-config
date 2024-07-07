bf1cc16062d7260d88d41e142a3be4d9d5baf4a9
Rasmus Eneman <rasmus@eneman.eu>
2024-07-07 11:03:12 +0200

    nvim: Colorscheme cleanup

--- home/base/config/nvim/lua/plugins/colorscheme.lua
+++ home/base/config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,21 @@
 return {
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000,
-    config = function()
-      require('catppuccin').setup {
-        background = { -- :h background
-          light = 'latte',
-          dark = 'frappe',
-        },
-        -- transparent_background = !vim.g.neovide,
-        transparent_background = true,
-        dim_inactive = { enable = true },
-      }
-      -- vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --   config = function()
+  --     require('catppuccin').setup {
+  --       background = { -- :h background
+  --         light = 'latte',
+  --         dark = 'frappe',
+  --       },
+  --       -- transparent_background = !vim.g.neovide,
+  --       transparent_background = true,
+  --       dim_inactive = { enable = true },
+  --     }
+  --     -- vim.cmd.colorscheme 'catppuccin'
+  --   end,
+  -- },
 
   {
     'folke/tokyonight.nvim',
@@ -24,42 +24,20 @@ return {
     config = function()
       require('tokyonight').setup {
         style = 'storm',
+        transparent = true,
+        day_brightness = 0.4,
       }
       vim.cmd.colorscheme 'tokyonight'
     end,
   },
-
-  -- {
-  --   'sainnhe/everforest',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     -- Optionally configure and load the colorscheme
-  --     -- directly inside the plugin declaration.
-  --     vim.g.everforest_enable_italic = true
-  --     -- vim.g.everforest_background = 'medium'
-  --     vim.g.everforest_transparent_background = 1
-  --     -- vim.cmd.colorscheme 'everforest'
-  --   end,
-  -- },
-
-  -- {
-  --   'rose-pine/neovim',
-  --   name = 'rose-pine',
-  --   priority = 1000,
-  --   config = function()
-  --     require('rose-pine').setup { styles = { transparency = true } }
-  --     vim.cmd.colorscheme 'rose-pine'
-  --   end,
-  -- },
 
   {
     'SyedFasiuddin/theme-toggle-nvim',
     config = function()
       require('theme-toggle-nvim').setup {
         colorscheme = {
-          light = 'catppuccin',
-          dark = 'catppuccin',
+          light = 'tokyonight',
+          dark = 'tokyonight',
           -- light = 'everforest',
           -- dark = 'everforest',
         },
