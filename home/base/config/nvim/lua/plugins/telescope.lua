161ea700a8a2486439057e9adc4d99214abaffa6
Rasmus Eneman <rasmus@eneman.eu>
2024-07-10 00:08:10 +0200

    nvim: s/leap/flash/

--- home/base/config/nvim/lua/plugins/telescope.lua
+++ home/base/config/nvim/lua/plugins/telescope.lua
@@ -25,12 +25,31 @@ return {
     local actions = require 'telescope.actions'
     local trouble = require 'trouble.sources.telescope'
 
+    local function flash(prompt_bufnr)
+      require('flash').jump {
+        pattern = '^',
+        label = { after = { 0, 0 } },
+        search = {
+          mode = 'search',
+          exclude = {
+            function(win)
+              return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= 'TelescopeResults'
+            end,
+          },
+        },
+        action = function(match)
+          local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+          picker:set_selection(match.pos[1] - 1)
+        end,
+      }
+    end
+
     require('telescope').setup {
       defaults = {
         hidden = true,
         mappings = {
-          i = { ['<c-t>'] = trouble.open_with_trouble },
-          n = { ['<c-t>'] = trouble.open_with_trouble, ['q'] = actions.close },
+          i = { ['<c-t>'] = trouble.open_with_trouble, ['<c-s>'] = flash },
+          n = { ['<c-t>'] = trouble.open_with_trouble, ['q'] = actions.close, s = flash },
         },
       },
       extensions = {
