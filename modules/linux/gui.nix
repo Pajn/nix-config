8c15225213ced5821868209b71263dc0480fad8e
Rasmus Eneman <rasmus@eneman.eu>
2024-07-13 11:33:38 +0200

    nixos: Make appimages work

--- modules/linux/gui.nix
+++ modules/linux/gui.nix
@@ -1,4 +1,4 @@
-{ user, pkgs, ... }:
+{ pkgs, ... }:
 {
   programs.firefox = {
     enable = true;
@@ -8,5 +8,13 @@
   };
   environment.sessionVariables = {
     MOZ_USE_XINPUT2 = "1";
+  };
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
   };
 }
